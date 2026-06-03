a849264e09edb19b53fd618f94b3befe8e1e4d87
iseeeva <107738269+iseeeva@users.noreply.github.com>
2026-06-04 00:27:12 +0300

    asas

--- src/components/udf/scripts/udf.ts
+++ src/components/udf/scripts/udf.ts
@@ -66,18 +66,15 @@ export interface IFieldAttributes extends IContentAttributes {
   fieldName: string
 }
 
-export interface IPageBreakAttributes {
+export interface IPageBreakAttributes {}
 
-}
-
-export interface IHeaderFooterAttributes extends ITextStyle {
-
-}
+export interface IHeaderFooterAttributes extends ITextStyle {}
 
 export interface IImageAttributes extends IContentAttributes {
   imageData: string
   height: number
   width: number
+  size?: number
 }
 
 export interface IParagraphAttributes {
@@ -209,33 +206,30 @@ class Utils {
     return !isNaN(parsed as number) ? (parsed as number) * 20 : 0
   }
 
-  public static docxNumberToNormal(value: string) {
+  public static docxNumberToNormal(value: string): number {
     const output = Number.parseFloat(value.slice(0, -2))
     return !isNaN(output) ? output : 0
   }
 
   public static convertColor(value?: number): string {
-    if (value == null || isNaN(value)) {
+    if (value == null || isNaN(value))
       return '#ffffff'
-    }
+
     let color = value
-    if (color < 0) {
+    if (color < 0)
       color = 0xFFFFFFFF + color + 1
-    }
+
     const r = (color >> 16) & 0xFF
     const g = (color >> 8) & 0xFF
     const b = color & 0xFF
-    return `#${r.toString(16).padStart(2, '0')}${g
-      .toString(16)
-      .padStart(2, '0')}${b.toString(16).padStart(2, '0')}`
+    return `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`
   }
 
   public static mergeObjects<T>(...styles: Partial<T>[]): T {
     return styles.reduce<Partial<T>>((acc, style) => {
       for (const key in style) {
-        if (acc[key as keyof T] == null && style[key as keyof T] != null) {
+        if (acc[key as keyof T] == null && style[key as keyof T] != null)
           acc[key as keyof T] = style[key as keyof T]
-        }
       }
       return acc
     }, {}) as T
@@ -265,236 +259,160 @@ class Utils {
 }
 
 class ToDocx {
-  public static parseDocument(document: IUdfDocument): docx.Document {
+  private referenceText: string = ''
+  private styles: { [name: string]: IGlobalStyle } = {}
+
+  private sectionProperties: docx.ISectionOptions['properties'] = undefined
+  private sectionChildren: docx.Paragraph[] = []
+  private sectionHeaders: docx.ISectionOptions['headers'] = undefined
+  private sectionFooters: docx.ISectionOptions['footers'] = undefined
+
+  public build(document: IUdfDocument): docx.Document {
     const { template } = document
     template.$$ ??= []
 
-    const paragraphs: Array<docx.Paragraph> = []
-    const sections: docx.ISectionPropertiesOptions[] = []
-    const styles: { [name: string]: IGlobalStyle } = {}
-    let referenceText: string = ''
+    this.parseTemplate(template)
 
-    for (const test of template.$$) {
-      switch (test['#name']) {
-        // Reference content
-        case NodeEnum.Content: {
-          const content = test
-          referenceText = content._
-        } break
-
-          // Page properties
-        case NodeEnum.Properties: {
-          const propertiesNode = test
-
-          if (propertiesNode.$$ == null) {
-            continue
-          }
-
-          for (const test of propertiesNode.$$) {
-            switch (test['#name']) {
-              case NodeEnum.PageFormat: {
-                const pageFmt = test
-
-                if (pageFmt.$ == null) {
-                  continue
-                }
-
-                const { paperOrientation, topMargin, rightMargin, bottomMargin, leftMargin } = pageFmt.$
-                sections.push({
-                  page: {
-                    size: { orientation: Orientations[paperOrientation] },
-                    margin: {
-                      top: `${(topMargin || Document.DEFAULT_MARGIN)}pt`,
-                      right: `${(rightMargin || Document.DEFAULT_MARGIN)}pt`,
-                      bottom: `${(bottomMargin || Document.DEFAULT_MARGIN)}pt`,
-                      left: `${(leftMargin || Document.DEFAULT_MARGIN)}pt`,
-                    },
-                  },
-                })
-
-                console.log('Page format configured.')
-              } break
-            }
-          }
-
-          console.log('Page properties configured.')
-        } break
-
-          // Pre-defined styles
-        case NodeEnum.Styles: {
-          const stylesNode = test
-
-          if (stylesNode.$$ == null) {
-            continue
-          }
-
-          stylesNode.$$?.forEach((group) => {
-            if (group?.$.name) {
-              styles[group.$.name] = group.$
-              console.log(`Loaded style: ${group.$.name}`)
-            }
-          })
-        } break
-      }
-    }
-
-    // Elements
     const elementsNode = template.$$.find(child => child['#name'] === NodeEnum.Elements)
     if (elementsNode == null)
       throw new Error('Document doesn\'t have elements')
 
     elementsNode.$$ ??= []
 
-    for (const test of elementsNode.$$)
-      paragraphs.push(...this.parseDocumentElement(test, referenceText, styles))
+    for (const el of elementsNode.$$)
+      this.sectionChildren.push(...this.parseDocumentElement(el))
 
-    // elementsNode.$$.forEach((el, eIdx) => {
-    //   const elementStyle = UDFUtils.mergeStyles<docx.IRunStylePropertiesOptions>(
-    //     UDFUtils.namedStyle(styles[el.$?.resolver ?? '']),
-    //     UDFUtils.namedStyle(styles[StyleDictionaryEnum.default]),
-    //   )
-
-    //   // Paragraphs
-    //   el.paragraph?.forEach((p, pIdx) => {
-    //     const para = new docx.Paragraph({
-    //       // tabStops: [
-    //       //   { position: 3000, type: "", leader: 'none' },
-    //       // ],
-    //       alignment: p.$?.Alignment ? AlignmentKeys[Number(p.$.Alignment) - 1] : undefined,
-    //       spacing: {
-    //         line: p.$?.LineSpacing && elementStyle.size
-    //           ? Math.round(UDFUtils.ptToTwip(UDFUtils.docxNumberToNormal(elementStyle.size) * (Number.parseFloat(p.$?.LineSpacing) + 1.0)))
-    //           : Math.round(UDFUtils.ptToTwip(UDFUtils.docxNumberToNormal(elementStyle.size) * 1.0)), // default line height * multiplier
-    //         lineRule: 'auto',
-    //       },
-    //       indent: {
-    //         firstLine: p.$?.FirstLineIndent ? `${Number.parseFloat(p.$.FirstLineIndent) / 28.346457}cm` : undefined,
-    //         left: p.$?.LeftIndent ? `${Number.parseFloat(p.$.LeftIndent) / 28.346457}cm` : undefined,
-    //         right: p.$?.RightIndent ? `${Number.parseFloat(p.$.RightIndent) / 28.346457}cm` : undefined,
-    //       },
-    //     })
-
-    //     p.content?.forEach((c) => {
-    //       const start = Number(c.$?.startOffset)
-    //       const length = Number(c.$?.length)
-    //       if (isNaN(start) || isNaN(length) || length <= 0)
-    //         return
-    //       const txt = textRef.substring(start, start + length)
-
-    //       const runStyle = UDFUtils.mergeStyles<docx.IRunStylePropertiesOptions>(
-    //         UDFUtils.textStyle(c),
-    //         UDFUtils.namedStyle(styles[p.$?.resolver ?? '']),
-    //         elementStyle,
-    //       )
-
-    //       para.addChildElement(new docx.TextRun({ text: txt, ...runStyle }))
-    //       this.log(`${start} ${p.$?.Alignment ? AlignmentKeys[Number(p.$.Alignment) - 1] : undefined} ${txt}`)
-    //     })
-
-    //     paragraphs.push(para)
-    //     this.log(`Parsed paragraph ${eIdx}.${pIdx}`)
-    //   })
-
-    //   // Tables
-    //   el.table?.forEach((tbl, tIdx) => {
-    //     const rows: docx.TableRow[] = []
-    //     tbl.row?.forEach((r, rIdx) => {
-    //       const cells = r.cell?.map((cell) => {
-    //         const cellParas = cell.paragraph?.map((cp) => {
-    //           const childRuns = cp.content?.map((cnt) => {
-    //             const start = Number(cnt.$?.startOffset)
-    //             const length = Number(cnt.$?.length)
-    //             const text = textRef.substring(start, start + length)
-    //             const style = UDFUtils.mergeStyles<docx.IRunStylePropertiesOptions>(
-    //               UDFUtils.textStyle(cnt),
-    //               UDFUtils.namedStyle(styles[cp.$?.resolver ?? '']),
-    //               UDFUtils.namedStyle(styles[StyleDictionaryEnum.default]),
-    //             )
-
-    //             return new docx.TextRun({ text, ...style })
-    //           }) ?? []
-
-    //           return new docx.Paragraph({ children: childRuns })
-    //         }) ?? []
-
-    //         return new docx.TableCell({ children: cellParas })
-    //       }) ?? []
-
-    //       rows.push(new docx.TableRow({ children: cells }))
-    //     })
-
-    //     paragraphs.push(new docx.Table({ rows }))
-    //     this.log(`Parsed table ${eIdx}.${tIdx}`)
-    //   })
-    // })
-
-    const doc = new docx.Document({
-      sections: [{ properties: sections[0], children: paragraphs }],
+    return new docx.Document({
+      sections: [{
+        properties: this.sectionProperties,
+        headers: this.sectionHeaders,
+        footers: this.sectionFooters,
+        children: this.sectionChildren,
+      }],
     })
-
-    return doc
   }
 
-  public static parseDocumentElement(
-    element: DocumentElement,
-    referenceText: string,
-    styles: { [name: string]: IGlobalStyle },
-  ): docx.Paragraph[] {
+  private parseTemplate(template: Template): void {
+    template.$$ ??= []
+
+    for (const node of template.$$) {
+      switch (node['#name']) {
+        case NodeEnum.Content:
+          this.referenceText = node._
+          break
+
+        case NodeEnum.Properties:
+          this.parseProperties(node)
+          break
+
+        case NodeEnum.Styles:
+          this.parseStyles(node)
+          break
+      }
+    }
+  }
+
+  private parseProperties(node: Properties): void {
+    if (node.$$ == null)
+      return
+
+    for (const prop of node.$$) {
+      switch (prop['#name']) {
+        case NodeEnum.PageFormat:
+          this.parsePageFormat(prop)
+          break
+      }
+    }
+  }
+
+  private parsePageFormat(prop: PageFormat): void {
+    if (prop.$ == null)
+      return
+
+    const { paperOrientation, topMargin, rightMargin, bottomMargin, leftMargin } = prop.$
+
+    this.sectionProperties = {
+      page: {
+        size: { orientation: Orientations[paperOrientation] },
+        margin: {
+          top: `${topMargin || Document.DEFAULT_MARGIN}pt`,
+          right: `${rightMargin || Document.DEFAULT_MARGIN}pt`,
+          bottom: `${bottomMargin || Document.DEFAULT_MARGIN}pt`,
+          left: `${leftMargin || Document.DEFAULT_MARGIN}pt`,
+        },
+      },
+    }
+  }
+
+  private parseStyles(node: Styles): void {
+    if (node.$$ == null)
+      return
+
+    for (const style of node.$$) {
+      if (style?.$.name)
+        this.styles[style.$.name] = style.$
+    }
+  }
+
+  private parseDocumentElement(element: DocumentElement): docx.Paragraph[] {
     const paragraphs: docx.Paragraph[] = []
 
     switch (element['#name']) {
       case NodeEnum.Paragraph:
-        paragraphs.push(this.parseParagraph(element, referenceText, styles))
+        paragraphs.push(this.parseParagraph(element))
         break
 
       case NodeEnum.PageBreak:
-        paragraphs.push(new docx.Paragraph({ children: [new docx.PageBreak(), ...element.$$?.map(elem1 => this.parseDocumentElement(elem1, referenceText, styles)).flat() || []] }))
+        paragraphs.push(new docx.Paragraph({
+          children: [
+            new docx.PageBreak(),
+            ...element.$$?.flatMap(elem1 => this.parseDocumentElement(elem1)) ?? [],
+          ],
+        }))
         break
 
       case NodeEnum.Table:
         break
 
-      case NodeEnum.Footer:
-        paragraphs.push(...element.$$?.map(elem1 => this.parseDocumentElement(elem1, referenceText, styles)).flat() || [])
-        break
+      case NodeEnum.Header: {
+        const children = element.$$?.flatMap(elem1 => this.parseDocumentElement(elem1)) ?? []
+        this.sectionHeaders = { default: new docx.Header({ children }) }
+      } break
+
+      case NodeEnum.Footer: {
+        const children = element.$$?.flatMap(elem1 => this.parseDocumentElement(elem1)) ?? []
+        this.sectionFooters = { default: new docx.Footer({ children }) }
+      } break
     }
 
     return paragraphs
   }
 
-  public static parseContent(
+  private parseContent(
     node: Content,
-    referenceText: string,
     paragraphStyle: docx.IRunStylePropertiesOptions,
   ): docx.TextRun {
-    const content = node
-    const start = Number(content.$?.startOffset)
-    const length = Number(content.$?.length)
+    const start = Number(node.$?.startOffset)
+    const length = Number(node.$?.length)
 
-    if (isNaN(start) || isNaN(length) || length <= 0 || start + length > referenceText.length) {
+    if (isNaN(start) || isNaN(length) || length <= 0 || start + length > this.referenceText.length)
       throw new Error('Paragraph content skipped due wrong offset/length')
-    }
 
-    const txt = referenceText.substring(start, start + length)
+    const txt = this.referenceText.substring(start, start + length)
     const runStyle = Utils.mergeObjects<docx.IRunStylePropertiesOptions>(
-      Utils.textStyle(content.$),
+      Utils.textStyle(node.$),
       paragraphStyle,
     )
 
-    return new docx.TextRun({ text: txt, ...runStyle, strike: content.$.strikethrough })
+    return new docx.TextRun({ text: txt, ...runStyle, strike: node.$.strikethrough })
   }
 
-  public static parseParagraph(
-    node: Paragraph,
-    referenceText: string,
-    styles: { [name: string]: IGlobalStyle },
-  ): docx.Paragraph {
-    const paragraph = node
-    paragraph.$$ ??= []
+  private parseParagraph(node: Paragraph): docx.Paragraph {
+    node.$$ ??= []
 
     const paragraphStyle = Utils.mergeObjects<docx.IRunStylePropertiesOptions>(
-      Utils.namedStyle(styles[paragraph.$?.resolver]),
-      Utils.namedStyle(styles[GlobalStyleEnum.hvl_default]),
+      Utils.namedStyle(this.styles[node.$?.resolver]),
+      Utils.namedStyle(this.styles[GlobalStyleEnum.hvl_default]),
     )
 
     const baseSize: number = typeof paragraphStyle.size === 'string'
@@ -502,27 +420,24 @@ class ToDocx {
       : (paragraphStyle.size || 11)
 
     const docxParagraph = new docx.Paragraph({
-      // tabStops: [
-      //   { position: 3000, type: "", leader: 'none' },
-      // ],
-      alignment: Alignments[paragraph.$?.Alignment ? (paragraph.$!.Alignment - 1) as AlignmentEnum : AlignmentEnum.left],
+      alignment: Alignments[node.$?.Alignment ? (node.$!.Alignment - 1) as AlignmentEnum : AlignmentEnum.left],
       spacing: {
-        line: paragraph.$?.LineSpacing != null
-          ? Math.round(Utils.ptToTwip(baseSize * (paragraph.$!.LineSpacing + 1.0)))
-          : Math.round(Utils.ptToTwip(baseSize)), // default line height * multiplier
+        line: node.$?.LineSpacing != null
+          ? Math.round(Utils.ptToTwip(baseSize * (node.$!.LineSpacing + 1.0)))
+          : Math.round(Utils.ptToTwip(baseSize)),
         lineRule: 'auto',
       },
       indent: {
-        firstLine: paragraph.$?.FirstLineIndent ? `${paragraph.$.FirstLineIndent}pt` : undefined,
-        left: paragraph.$?.LeftIndent ? `${paragraph.$.LeftIndent}pt` : undefined,
-        right: paragraph.$?.RightIndent ? `${paragraph.$.RightIndent}pt` : undefined,
+        firstLine: node.$?.FirstLineIndent ? `${node.$.FirstLineIndent}pt` : undefined,
+        left: node.$?.LeftIndent ? `${node.$.LeftIndent}pt` : undefined,
+        right: node.$?.RightIndent ? `${node.$.RightIndent}pt` : undefined,
       },
     })
 
-    for (const test of paragraph.$$) {
-      switch (test['#name']) {
+    for (const child of node.$$) {
+      switch (child['#name']) {
         case NodeEnum.Content:
-          docxParagraph.addChildElement(this.parseContent(test, referenceText, paragraphStyle))
+          docxParagraph.addChildElement(this.parseContent(child, paragraphStyle))
           break
 
         case NodeEnum.Tab:
@@ -531,19 +446,15 @@ class ToDocx {
           break
 
         case NodeEnum.Image:
-          {
-            const imageOptions: docx.IImageOptions = {
-              data: Uint8Array.from(window.atob(test.$.imageData), c => c.charCodeAt(0)),
-              type: 'png', // TODO: type check needs
-              transformation: {
-                height: test.$.size ? test.$.size : test.$.height,
-                width: test.$.size ? test.$.size : test.$.width,
-              },
-            }
-
-            const imageObject = new docx.ImageRun(imageOptions)
-            docxParagraph.addChildElement(imageObject)
-          } break
+          docxParagraph.addChildElement(new docx.ImageRun({
+            data: Uint8Array.from(window.atob(child.$.imageData), c => c.charCodeAt(0)),
+            type: 'png',
+            transformation: {
+              height: child.$.size ?? child.$.height,
+              width: child.$.size ?? child.$.width,
+            },
+          }))
+          break
       }
     }
 
@@ -573,15 +484,13 @@ export class Document {
     let parsed: IUdfDocument | undefined
 
     parser.parseString(xmlString, (err, result: IUdfDocument) => {
-      if (err) {
+      if (err)
         throw new Error(`Failed to parse UDF XML: ${err.message}`)
-      }
       parsed = result
     })
 
-    if (!parsed) {
+    if (!parsed)
       throw new Error('XML parsing resulted in undefined document.')
-    }
 
     this.validateVersion(parsed)
     return parsed
@@ -590,20 +499,18 @@ export class Document {
   public static validateVersion(document: IUdfDocument): void {
     try {
       const version = document.template?.$?.format_id?.toString()
-      if (!version) {
+      if (!version)
         throw new Error('Missing UDF format version.')
-      }
 
-      if (!Object.values(Version).includes(version as Version)) {
+      if (!Object.values(Version).includes(version as Version))
         throw new Error(`Unsupported UDF Version: ${version}`)
-      }
     }
     catch (error) {
       throw new Error(`Failed during UDF version validation: ${(error as Error).message}`)
     }
   }
 
-  public toDocx() {
-    return ToDocx.parseDocument(this.document)
+  public toDocx(): docx.Document {
+    return new ToDocx().build(this.document)
   }
 }
