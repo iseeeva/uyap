0bce74109907cc22988abef5da982a46c4e5c27f
iseeeva <107738269+iseeeva@users.noreply.github.com>
2026-08-02 22:31:08 +0300

    build fixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,33 @@ import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: 'uyap',
+export default defineConfig(({
+  base: '/uyap',
   plugins: [
     vue(),
     vueDevTools(),
+    nodePolyfills({
+      include: ['timers', 'stream'],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-vendor': ['vue', 'vue-router'],
+          'xml2js': ['xml2js'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
+  },
+}))
